840af622ff6e75b4738169a1d79e851cd93b62ef
Jon Hawkesworth <jhawkesworth@users.noreply.github.com>
2023-03-25 16:39:04 +0000

    got relative path working.  Much more satisfactory!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate rocket;
-use rocket::fs::FileServer;
+use rocket::fs::{FileServer, relative};
 use std::path::PathBuf;
 
 #[get("/")]
@@ -14,9 +14,9 @@ fn index() -> &'static str {
 async fn rocket(#[shuttle_static_folder::StaticFolder] _static_folder: PathBuf) -> shuttle_rocket::ShuttleRocket {
 
     let rocket = rocket::build().mount("/hello", routes![index])
-        .mount("/public", FileServer::from("/opt/shuttle/shuttle-builds/jh-rocket-fs/static"))
+        //.mount("/public", FileServer::from("/opt/shuttle/shuttle-builds/jh-rocket-fs/static"))
         // ^ if do this, must first symlink the folder so you can cargo shuttle run locally
-        ;
+        .mount("/public", FileServer::from(relative!("static")));
 
     Ok(rocket.into())
 }
