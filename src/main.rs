d16bc08358ca54829876eb5f1587145bd8294a7d
Jon Hawkesworth <jhawkesworth@users.noreply.github.com>
2023-03-25 16:27:02 +0000

    initial checkin of basic example where the rocket init demo app has had ability to serve any static content from 'static' folder to /public. Its unlovely as it has name of project embedded in the path.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate rocket;
+use rocket::fs::FileServer;
+use std::path::PathBuf;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+
+// https://docs.rs/rocket/0.5.0-rc.2/rocket/fs/struct.FileServer.html#example
+#[shuttle_runtime::main]
+async fn rocket(#[shuttle_static_folder::StaticFolder] _static_folder: PathBuf) -> shuttle_rocket::ShuttleRocket {
+
+    let rocket = rocket::build().mount("/hello", routes![index])
+        .mount("/public", FileServer::from("/opt/shuttle/shuttle-builds/jh-rocket-fs/static"))
+        // ^ if do this, must first symlink the folder so you can cargo shuttle run locally
+        ;
+
+    Ok(rocket.into())
+}
